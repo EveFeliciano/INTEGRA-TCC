aba378c1c1f2f6f9cbab35e6a6bb48505526f9ee
Gabriel Caspirro Demarchi <124791810+GabrielCaspirro@users.noreply.github.com>
2025-09-01 20:04:03 -0300

    Add files via upload

--- integra.sql
+++ integra.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 05/08/2025 às 17:04
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Tempo de geração: 02/09/2025 às 01:03
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -101,6 +101,13 @@ CREATE TABLE `empresa` (
   `id_endereco` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `empresa`
+--
+
+INSERT INTO `empresa` (`id_empresa`, `nome`, `email`, `senha`, `telefone`, `data_cadastro`, `setor`, `cnpj`, `id_endereco`) VALUES
+(5, 'TechPlusPlus Ltda', 'contato@techplusplus.com.br', '$2b$10$We53Eu2akj28CI3a/7d2t.I3zvIMynFOY92fqeHbWghTz7fItjHJa', '(11) 4022-8922', '2025-08-19 00:17:23', 'Tecnologia', '12.345.628/7771-90', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -117,6 +124,14 @@ CREATE TABLE `endereco` (
   `cep` varchar(9) DEFAULT NULL,
   `complemento` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `endereco`
+--
+
+INSERT INTO `endereco` (`id_endereco`, `rua`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `complemento`) VALUES
+(4, NULL, NULL, 'Ponte Seca', 'Ribeirão Pires', 'Sã', '09412-350', '(Jd Valentina)'),
+(5, 'Rua Ana Lacivita Amaral', '332', 'Ponte Seca', 'Ribeirão Pires', 'SP', '09412-350', '(Jd Valentina)');
 
 -- --------------------------------------------------------
 
@@ -185,7 +200,10 @@ CREATE TABLE `instituicao` (
   `nome` varchar(255) DEFAULT NULL,
   `id_endereco` int(11) DEFAULT NULL,
   `tipo` enum('ETEC','FATEC') DEFAULT NULL,
-  `cod_instituicao` varchar(3) DEFAULT NULL
+  `cod_instituicao` varchar(3) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -214,8 +232,41 @@ CREATE TABLE `palestrante` (
   `email` varchar(200) DEFAULT NULL,
   `senha` varchar(200) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL,
-  `bio` text DEFAULT NULL
+  `bio` text DEFAULT NULL,
+  `id_empresa` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `palestrante`
+--
+
+INSERT INTO `palestrante` (`id`, `nome`, `especialidade`, `email`, `senha`, `telefone`, `bio`, `id_empresa`) VALUES
+(2, 'Eduardo Souza', NULL, 'gabriel.gcd08@gmail.com', '$2b$10$4JaOrGBYnauGh8tpwWmyKOOKdvfhYB2nu3BBNXR/Fr9dUfWBzfZOW', '(11) 93030-3030', NULL, 5),
+(3, 'Eduardo Souza Master', NULL, 'gabriel.gcd08@gmail.com', '$2b$10$F3Kl331f3q9./h9XEpk78OVxmVXzaHSfoB/aPq5OnzuOLhzrdhzN.', '(11) 93030-3030', NULL, 5),
+(4, 'Eduardo Souza Master 2', NULL, 'gabriel.gcd08@gmail.com', '$2b$10$VbbHViGtdYLvSEMcbD2Cmu1xEgtco4z9q2/Q8kND20yGOSY1pTbmK', '(11) 93030-3030', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `solicitacao_palestrante`
+--
+
+CREATE TABLE `solicitacao_palestrante` (
+  `id` int(11) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `id_palestrante` int(11) NOT NULL,
+  `status` enum('Pendente','Aceito','Recusado') DEFAULT 'Pendente',
+  `data_solicitacao` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `solicitacao_palestrante`
+--
+
+INSERT INTO `solicitacao_palestrante` (`id`, `id_empresa`, `id_palestrante`, `status`, `data_solicitacao`) VALUES
+(1, 5, 2, 'Aceito', '2025-09-01 22:31:39'),
+(2, 5, 3, 'Aceito', '2025-09-01 22:48:03'),
+(3, 5, 4, 'Recusado', '2025-09-01 22:51:02');
 
 --
 -- Índices para tabelas despejadas
@@ -314,7 +365,16 @@ ALTER TABLE `log_evento`
 -- Índices de tabela `palestrante`
 --
 ALTER TABLE `palestrante`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_palestrante_empresa` (`id_empresa`);
+
+--
+-- Índices de tabela `solicitacao_palestrante`
+--
+ALTER TABLE `solicitacao_palestrante`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empresa` (`id_empresa`),
+  ADD KEY `id_palestrante` (`id_palestrante`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -348,13 +408,13 @@ ALTER TABLE `coordenador_instituicao`
 -- AUTO_INCREMENT de tabela `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `endereco`
 --
 ALTER TABLE `endereco`
-  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `evento`
@@ -396,7 +456,13 @@ ALTER TABLE `log_evento`
 -- AUTO_INCREMENT de tabela `palestrante`
 --
 ALTER TABLE `palestrante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `solicitacao_palestrante`
+--
+ALTER TABLE `solicitacao_palestrante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restrições para tabelas despejadas
@@ -453,6 +519,19 @@ ALTER TABLE `instituicao`
 --
 ALTER TABLE `log_evento`
   ADD CONSTRAINT `log_evento_ibfk_1` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`) ON DELETE CASCADE;
+
+--
+-- Restrições para tabelas `palestrante`
+--
+ALTER TABLE `palestrante`
+  ADD CONSTRAINT `fk_palestrante_empresa` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `solicitacao_palestrante`
+--
+ALTER TABLE `solicitacao_palestrante`
+  ADD CONSTRAINT `solicitacao_palestrante_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`) ON DELETE CASCADE,
+  ADD CONSTRAINT `solicitacao_palestrante_ibfk_2` FOREIGN KEY (`id_palestrante`) REFERENCES `palestrante` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
